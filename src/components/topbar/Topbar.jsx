2b043dd25c355518b1d7157be38c3f9a5c6ae85a
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-11-06 12:03:15 -0500

    setting up navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
     <div className="topbar">
-        
+        <div className="wrapper">
+          <div className="left">
+            <a href="#intro" className="logo">genius</a>
+            <div className="itemContainer">
+              <Person className="icon"/>
+              <span>226 972 6339</span>
+            </div>
+            <div className="itemContainer">
+              <Mail className="icon"/>
+              <span>roopraim@mcmaster.ca</span>
+            </div>
+          </div>
+          <div className="right">
+            <div className="hamburger">
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
