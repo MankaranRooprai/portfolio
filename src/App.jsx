105bdb6ff886039c4021e298bb5b3debe57fe483
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-11-09 21:55:08 -0500

    changed folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
+import Intro from "./components/introFolder/Intro";
 //import Portfolio from "./components/portfolio/Portfolio";
 //import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
