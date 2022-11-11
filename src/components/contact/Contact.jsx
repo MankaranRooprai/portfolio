85e4340e912c25b3d81b84db6379ff84e77c080c
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-11-10 20:26:27 -0500

    changed menu, and added emailjs functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
 
-  const handleSubmit = (e) => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setMessage(true);
+  // };
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
     setMessage(true);
-  };
+
+    emailjs.sendForm('service_r06nvn9', 'template_ku32ifd', form.current, 'IK-kag2H2823g1tCM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -15,9 +32,10 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Email" name="from_email" />
+          <input type="text" placeholder="Name" name="from_name" />
+          <textarea placeholder="Message" name="message"></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
